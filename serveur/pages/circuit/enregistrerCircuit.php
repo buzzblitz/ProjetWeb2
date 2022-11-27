cd93c01e7185cd95daff36800ce5eeac17bcd059
FelixA02 <56594071+FelixA02@users.noreply.github.com>
2022-11-27 15:38:30 -0500

    enregistrer circuit

--- serveur/pages/circuit/enregistrerCircuit.php
+++ serveur/pages/circuit/enregistrerCircuit.php
@@ -1,18 +1,40 @@
-<?php
-    require_once("../../includes/bdconfig.inc.php");
-	$nom=$_POST['nom']; 
-	$desc=$_POST['desc'];
-	$categ=$_POST['categ'];
-	$prix = $_POST['prix'];
-	$qted = $_POST['qted'];
-	$seuil = $_POST['seuil'];
-	$dossier="../../images_articles/";
-	$image="avatar.png";
-	if($_FILES['img']['tmp_name']!==""){
+<?php 
+    //Établir connexion à la BD
+    require_once("../includes/bdconfig.inc.php");
+    $msg="Membre+bien+enregistré.";
+    //Recupérer les données du formulaire
+    //donner circuit
+    $nomc = $_POST['nomc'];
+    $img = $_POST['imgc'];
+    $descc = $_POST['descc'];
+    $categ = $_POST['categ'];
+
+    //donner etape
+    $etape = $_POST['etape'];
+    $imge = $_POST['imge'];
+    $desce = $_POST['desce'];
+    $dated = $_POST['dated'];
+    $datef = $_POST['datef'];
+    $lieud = $_POST['lieud'];
+
+
+    //donner journee
+    $datej = $_POST['datej'];
+    $autre = $_POST['autre'];
+
+    //donner activiter
+    $noma = $_POST['noma'];
+    $heuredebut = $_POST['heuredebut'];
+    $heurefin = $_POST['heurefin'];
+    $descea = $_POST['descea'];
+
+    $dossier="photos/";
+	$image="avatar.jpg";
+	if($_FILES['photo']['tmp_name']!==""){
 		$nomImage=sha1($nom.time());
 		//Upload de la photo
-		$tmp = $_FILES['img']['tmp_name'];
-		$fichier= $_FILES['img']['name'];
+		$tmp = $_FILES['photo']['tmp_name'];
+		$fichier= $_FILES['photo']['name'];
 		$extension=strrchr($fichier,'.');
 		@move_uploaded_file($tmp,$dossier.$nomImage.$extension);
 		// Enlever le fichier temporaire chargé
@@ -20,14 +42,21 @@
 		$image=$nomImage.$extension;
 	}
 	try{
-		$requete="INSERT INTO articles values(0,?,?,?,?,?,?,?)";
-		$stmt = $connexion->prepare($requete);
-		$stmt->bind_param("ssssiid", $nom,$desc,$image,$categ,$qted,$seuil,$prix); //Selon l'ordre des colonnes de la table categories
-		$stmt->execute();
+		$requete = "INSERT INTO membres VAlUES (0,?,?,?,?,?,?)";
+        $stmt = $connexion->prepare($requete);
+        $stmt->bind_param("ssssss", $prenom,$nom,$courriel,$sexe,$daten,$image);
+        $stmt->execute();
+        $idm = $connexion->insert_id;
+
+        $requete = "INSERT INTO connexion VAlUES (?,?,?,'A','M')";
+        $stmt = $connexion->prepare($requete);
+        $stmt->bind_param("iss", $idm,$courriel,$pass);
+        $stmt->execute();
 	} catch(Exception $e){
 		//Retourner le message voulu
+        $msg="Problème+pour+enregistré+le+membre.";
 	}finally {
 		mysqli_close($connexion);
-	}
-	header('Location: ../admin/admin.php?msg=Article+a+été+enregistré');
+        header('Location: ../pages/membre.php');
+    }
 ?>
