a036631876462ddf2a8fbd86b75035f407f5c533
FelixA02 <56594071+FelixA02@users.noreply.github.com>
2022-11-27 13:17:32 -0500

    Update admin.php

--- serveur/pages/admin.php
+++ serveur/pages/admin.php
@@ -169,9 +169,70 @@ require_once("../includes/menu_admin.inc.php");
                                 <option value="Dep">Deploiement</option>
                             </select>
                         </div>
-                        <div class="personal-details">
+                        <div class='col-md-12' id='etapes'>
+        <div class='col-md-12'>
+                            <label for='etape' class='form-label'>Etape</label>
+                            <input type='text' class='form-control' id='etape' name='etape' value=''>
                         </div>
-                        <button type="button" class="add-row" id="defaultBoutonStart"  onclick="ajouterDefault(0)">+</button>
+                        <div class='col-md-12'>
+                            <label for='imge' class='form-label'>Image de l'Etape</label>
+                            <input type='file' class='form-control' id='imge' name='imge' value=''>
+                        </div>
+                        <div class='col-md-12'>
+                            <label for='desce' class='form-label'>Description de l'Etape</label>
+                            <input type='text' class='form-control' id='desce' name='desce' value=''>
+                        </div>
+                        <div class='col-md-6'>
+                        <label for='dated' class='form-label'>Date du Debut</label>
+                            <input type='date' class='form-control is-valid' id='dated' name='dated'>
+                        </div>
+                        <div class='col-md-12'>
+                        </div>
+                        <div class='col-md-6'>
+                            <label for='datef' class='form-label'>Date de la Fin</label>
+                            <input type='date' class='form-control is-valid' id='datef' name='datef'>
+                        </div>
+                        <div class='col-md-12'>
+                            <label for='lieud' class='form-label'>Lieu de rencontre pour le Diner</label>
+                            <input type='text' class='form-control' id='lieud' name='lieud' value=''>
+                        </div>
+
+            <div class='col-md-12' id='journees'>
+            <div class='col-md-12'>
+                            <label for='journee' class='form-label'>Journee</label>
+                            </div>
+                            <div class='col-md-6'>
+                                <label for='datej' class='form-label'>Date</label>
+                                <input type='date' class='form-control is-valid' id='datej' name='datej'>
+                            </div>
+                            <div class='col-md-12'>
+                                <label for='autre' class='form-label'>Autre information</label>
+                                <input type='text' class='form-control' id='autre' name='autre' value='' >
+                            </div>
+
+                <div class='col-md-12' id='activiters'>
+                    <div class='col-md-12'>
+                    <label for='noma' class='form-label'>Nom de l'activiter</label>
+                                        <input type='text' class='form-control' id='noma' name='noma' value=''>
+                                    </div>
+
+                                    <div class='col-md-6'>
+                                        "<label for='heuredebut' class='form-label'>Heure du debut de l'activiter</label>
+                                        "<input type='time' class='form-control is-valid' id='heuredebut' name='heuredebut'>
+                                    </div>
+                                    <div class='col-md-12'>
+                                    </div>
+                                    <div class='col-md-6'>
+                                        <label for='heurefin' class='form-labe'>Heure de fin de l'activiter</label>
+                                        <input type='time' class='form-control is-valid' id='heurefin' name='heurefin'>
+                                    </div>
+                                    <div class='col-md-12'>
+                                        <label for='desca' class='form-label'>Description des activiter</label>
+                                        <input type='text' class='form-control' id='descea' name='descea' value=''>
+                                    </div>
+                                    </div>
+                                    </div>
+                                    </div>
                         <div class="col-12">
                             <button class="btn btn-primary" type="submit">Enregistrer</button>
                         </div>
@@ -291,280 +352,3 @@ require_once("../includes/menu_admin.inc.php");
 </body>
 
 </html>
-
-<script>
-
-    var etapes = 1;
-    var journee = 1;
-    var activiter = 1; 
-
-    //valeur qui determine combien d'etapes, journee et activiter vont etre cree a la creation de la page
-    var nbEtapes = 5;
-    var nbJournees = 7;
-    var nbActiviters = 4;
-
-
-
-    function ajouterDefault(x) {
-    //code pour faire apparaitre une nouvelle etape
-    document.getElementById("etapes" + (x + 1)).removeAttribute("hidden");
-    document.getElementById("etape" + (x + 1)).setAttribute("required", "");
-    document.getElementById("desce" + (x + 1)).setAttribute("required", "");
-    document.getElementById("lieud" + (x + 1)).setAttribute("required", "");
-    document.getElementById("defaultBoutonStart").setAttribute("hidden", "");
-
-    //code pour faire apparaitre la premier journee de l'etape
-    document.getElementById("journees" + ((1% (x + 1) * x * nbJournees)+ 1)).removeAttribute("hidden");
-    document.getElementById("autre" + ((1% (x + 1) * x * nbJournees)+ 1)).setAttribute("required", "");
-
-    //code pour faire apparaitre la premiere activiter de la journee
-    document.getElementById("activiters" + ((1% (x + 1) * x * (nbJournees * nbActiviters))+ 1)).removeAttribute("hidden");
-    document.getElementById("noma" + ((1% (x + 1) * x * (nbJournees * nbActiviters))+ 1)).setAttribute("required", "");
-    document.getElementById("descea" + ((1% (x + 1) * x * (nbJournees * nbActiviters))+ 1)).setAttribute("required", "");
-    
-    }
-
-
-    function ajouterEtape(x) {
-    //code pour faire apparaitre une nouvelle etape
-    document.getElementById("etapes" + (x + 1)).removeAttribute("hidden");
-    document.getElementById("etape" + (x + 1)).setAttribute("required", "");
-    document.getElementById("desce" + (x + 1)).setAttribute("required", "");
-    document.getElementById("lieud" + (x + 1)).setAttribute("required", "");
-    document.getElementById("bouttonEtapesAjouter" + x).setAttribute("hidden", "");
-
-    //code pour faire apparaitre la premier journee de l'etape
-    document.getElementById("journees" + ((1% (x + 1) * x * nbJournees)+ 1)).removeAttribute("hidden");
-    document.getElementById("autre" + ((1% (x + 1) * x * nbJournees)+ 1)).setAttribute("required", "");
-
-    //code pour faire apparaitre la premiere activiter de la journee
-    document.getElementById("activiters" + ((1% (x + 1) * x * (nbJournees * nbActiviters))+ 1)).removeAttribute("hidden");
-    document.getElementById("noma" + ((1% (x + 1) * x * (nbJournees * nbActiviters))+ 1)).setAttribute("required", "");
-    document.getElementById("descea" + ((1% (x + 1) * x * (nbJournees * nbActiviters))+ 1)).setAttribute("required", "");
-    
-    }
-
-    function ajouterJournee(x) {
-
-    //code pour faire apparaitre une nouvelle journee
-    document.getElementById("journees" + (x + 1)).removeAttribute("hidden");
-    document.getElementById("autre" + (x + 1)).setAttribute("required", "");
-    document.getElementById("bouttonJourneesAjouter" + x).setAttribute("hidden", "");
-
-    //code pour faire apparaitre la premiere activiter de la journee
-    document.getElementById("activiters" + ( (x)*nbActiviters+1 )).removeAttribute("hidden");
-    document.getElementById("noma" + ( (x)*nbActiviters+1 )).setAttribute("required", "");
-    document.getElementById("descea" + ( (x)*nbActiviters+1 )).setAttribute("required", "");
-    
-    }
-
-    function ajouterActiviter(x) {
-    //code pour faire apparaitre la premiere activiter de la journee
-    document.getElementById("activiters" + (x + 1)).removeAttribute("hidden");
-    document.getElementById("noma" + (x + 1)).setAttribute("required", "");
-    document.getElementById("descea" + (x + 1)).setAttribute("required", "");
-    document.getElementById("bouttonActivitersAjouter" + x).setAttribute("hidden", "");
-
-    //TODO: une erreur est lever parce que dans la premier valeur le bouton enlever n'existe pas
-    document.getElementById("bouttonActivitersEnlever" + x).setAttribute("hidden", "");
-
-    }
-
-
-
-    function enleverJournee(x) {
-
-    //code pour faire apparaitre une nouvelle journee
-    document.getElementById("journees" + x).setAttribute("hidden", "");
-    document.getElementById("autre" + x).removeAttribute("required");
-    document.getElementById("bouttonJourneesAjouter" + (x - 1)).removeAttribute("hidden");
-
-    document.getElementById("datej" + x).value = document.getElementById("datej" + x).defaultValue;    
-    document.getElementById("autre" + x).value = document.getElementById("autre" + x).defaultValue;    
-
-
-    for ($j = 1; $j <= nbActiviters; $j++) {
-
-    document.getElementById("activiters" + ( (x-1)*nbActiviters+ $j ) ).setAttribute("hidden", "");
-    document.getElementById("noma" + ( (x-1)*nbActiviters+ $j )).removeAttribute("required");
-    document.getElementById("descea" + ( (x-1)*nbActiviters+ $j )).removeAttribute("required");
-
-    document.getElementById("noma" + ( (x-1)*nbActiviters+ $j )).value = document.getElementById("noma" + ( (x-1)*nbActiviters+ $j )).defaultValue;
-    document.getElementById("heuredebut" + ( (x-1)*nbActiviters+ $j )).value = document.getElementById("heuredebut" + ( (x-1)*nbActiviters+ $j )).defaultValue;
-    document.getElementById("heurefin" + ( (x-1)*nbActiviters+ $j )).value = document.getElementById("heurefin" + ( (x-1)*nbActiviters+ $j )).defaultValue;
-    document.getElementById("descea" + ( (x-1)*nbActiviters+ $j )).value = document.getElementById("descea" + ( (x-1)*nbActiviters+ $j )).defaultValue;
-
-    document.getElementById("bouttonActivitersAjouter" + ( (x-1)*nbActiviters+ $j )).removeAttribute("hidden");
-
-    if($j != 1) {
-        //todo a regler lerreu qui arrive a cette etape
-        document.getElementById("bouttonActivitersEnlever" + ( (x-1)*nbActiviters+ $j )).removeAttribute("hidden");
-        }
-    }
-
-    }
-
-
-    
-    function enleverActiviter(x) {
-    //code pour faire apparaitre la premiere activiter de la journee
-    document.getElementById("activiters" + x ).setAttribute("hidden", "");
-    document.getElementById("noma" + x).removeAttribute("required");
-    document.getElementById("descea" + x).removeAttribute("required");
-
-    document.getElementById("noma" + x).value = document.getElementById("noma" + x).defaultValue;
-    document.getElementById("heuredebut" + x).value = document.getElementById("heuredebut" + x).defaultValue;
-    document.getElementById("heurefin" + x).value = document.getElementById("heurefin" + x).defaultValue;
-    document.getElementById("descea" + x).value = document.getElementById("descea" + x).defaultValue;
-
-
-    document.getElementById("bouttonActivitersAjouter" + (x - 1)).removeAttribute("hidden");
-    document.getElementById("bouttonActivitersEnlever" + (x - 1)).removeAttribute("hidden");
-
-    }
-
-
-    $(document).ready(function(){
-    var string = ""
-    var $clone = $( "div.personal-details" ).first();
-    for ($y = 1; $y <= nbEtapes; $y++) {
-        string +=
-        "<div class='col-md-12' hidden id='etapes" + etapes + "'>" +
-        "<div class='col-md-12'>" +
-                            "<label for='etape" + etapes + "' class='form-label'>Etape</label>" +
-                            "<input type='text' class='form-control' id='etape" + etapes + "' name='etape" + etapes + "' value=''>" +
-                        "</div>" +
-                        "<div class='col-md-12'>" +
-                            "<label for='imge" + etapes + "' class='form-label'>Image de l'Etape</label>" +
-                            "<input type='file' class='form-control' id='imge" + etapes + "' name='imge" + etapes + "' value=''>" +
-                        "</div>" +
-                        "<div class='col-md-12'>" +
-                            "<label for='desce" + etapes + "' class='form-label'>Description de l'Etape</label>" +
-                            "<input type='text' class='form-control' id='desce" + etapes + "' name='desce" + etapes + "' value=''>" +
-                        "</div>" +
-                        "<div class='col-md-6'>" +
-                            "<label for='dated" + etapes + "' class='form-label'>Date du Debut</label>" +
-                            "<input type='date' class='form-control is-valid' id='dated" + etapes + "' name='dated" + etapes + "'>" +
-                        "</div>" +
-                        "<div class='col-md-12'>" +
-                        "</div>" +
-                        "<div class='col-md-6'>" +
-                            "<label for='datef" + etapes + "' class='form-label'>Date de la Fin</label>" +
-                            "<input type='date' class='form-control is-valid' id='datef" + etapes + "' name='datef" + etapes + "'>" +
-                        "</div>" +
-                        "<div class='col-md-12'>" +
-                            "<label for='lieud" + etapes + "' class='form-label'>Lieu de rencontre pour le Diner</label>" +
-                            "<input type='text' class='form-control' id='lieud" + etapes + "' name='lieud" + etapes + "' value=''>" +
-                        "</div>";
-        if($y != nbEtapes) {
-            string += "<button type='button' id='bouttonEtapesAjouter" + etapes + "' onclick='ajouterEtape(" + etapes + ")' >+</button>"
-        }
-        for ($x = 1; $x <= nbJournees; $x++) {
-            string += 
-            "<div class='col-md-12' hidden id='journees" + journee + "'>" +
-            "<div class='col-md-12'>" +
-                                "<label for='journee" + journee + "' class='form-label'>Journee</label>" +
-                            "</div>" +
-                            "<div class='col-md-6'>" +
-                                "<label for='datej" + journee + "' class='form-label'>Date</label>" + 
-                                "<input type='date' class='form-control is-valid' id='datej" + journee + "' name='datej" + journee + "'>" +
-                            "</div>" +
-                            "<div class='col-md-12'>" +
-                                "<label for='autre" + journee + "' class='form-label'>Autre information</label>" +
-                                "<input type='text' class='form-control' id='autre" + journee + "' name='autre" + journee + "' value='' >" +
-                            "</div>";
-            ;
-            if($x != nbJournees) {
-            string += "<button type='button' id='bouttonJourneesAjouter" + journee + "' onclick='ajouterJournee(" + journee + ")' >+</button>"
-            }
-            if($x != 1) {
-                    string += "<button type='button' id='bouttonJourneesEnlever" + journee + "' onclick='enleverJournee(" + journee + ")' >-</button>"
-                }
-            for ($i = 1; $i <= nbActiviters; $i++) {
-                string +=
-                "<div class='col-md-12' hidden id='activiters" + activiter + "'>" +
-                    "<div class='col-md-12'>" +
-                    "<label for='noma" + activiter + "' class='form-label'>Nom de l'activiter</label>" +
-                                        "<input type='text' class='form-control' id='noma" + activiter + "' name='noma" + activiter + "' value=''>" +
-                                    "</div>" +
-
-                                    "<div class='col-md-6'>" +
-                                        "<label for='heuredebut" + activiter + "' class='form-label'>Heure du debut de l'activiter</label>" +
-                                        "<input type='time' class='form-control is-valid' id='heuredebut" + activiter + "' name='heuredebut" + activiter + "'>" +
-                                    "</div>" +
-                                    "<div class='col-md-12'>" +
-                                    "</div>" +
-                                    "<div class='col-md-6'>" +
-                                        "<label for='heurefin" + activiter + "' class='form-labe'>Heure de fin de l'activiter</label>" +
-                                        "<input type='time' class='form-control is-valid' id='heurefin" + activiter + "' name='heurefin" + activiter + "'>" +
-                                    "</div>" +
-                                    "<div class='col-md-12'>" +
-                                        "<label for='desca" + activiter + "' class='form-label'>Description des activiter</label>" +
-                                        "<input type='text' class='form-control' id='descea" + activiter + "' name='descea" + activiter + "' value=''>" +
-                                    "</div>";
-                if($i != nbActiviters) {
-                    string += "<button type='button' id='bouttonActivitersAjouter" + activiter + "' onclick='ajouterActiviter(" + activiter + ")' >+</button>"
-                }
-                if($i != 1) {
-                    string += "<button type='button' id='bouttonActivitersEnlever" + activiter + "' onclick='enleverActiviter(" + activiter + ")' >-</button>"
-                }
-                string += "</div>";
-                activiter++;
-            }
-            string += "</div>";
-            journee++;
-        }
-        string += "</div>";
-        etapes++;
-    }
-    $clone.append(string);
-    string = "";
-    $clone.insertBefore( ".add-row" );
-
-   /* $(document).on('click', '.add-row', function() {
-        //code pour faire apparaitre une etape
-        document.getElementById("etapes1").removeAttribute("hidden");
-        document.getElementById("etape1").setAttribute("required", "");
-        document.getElementById("desce1").setAttribute("required", "");
-        document.getElementById("lieud1").setAttribute("required", "");
-
-        //code pour faire apparaitre une journee
-        document.getElementById("journees1").removeAttribute("hidden");
-        document.getElementById("autre1").setAttribute("required", "");
-
-        //code pour faire apparaitre une acticiter
-        document.getElementById("activiters1").removeAttribute("hidden");
-        document.getElementById("noma1").setAttribute("required", "");
-        document.getElementById("descea1").setAttribute("required", "");
-    });*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   $(document).on('click', '.remove-row', function() {
-    $(this).parent().remove();
-    });
-
-
-    $(document).on('click', '.add-row-journee', function() {
-        var $clone = $( "div.personal-details-journee" ).first().clone();
-        $clone.append( "<button type='button' class='remove-row-journee'>-</button>" );
-        $clone.insertBefore( ".add-row" );
-    });
-    
-    $(document).on('click', '.remove-row-journee', function() {
-        $(this).parent().remove();
-    });
-});
-</script>
