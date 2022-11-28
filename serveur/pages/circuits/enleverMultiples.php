86ae1c145cbf5bb10bed085148876009477279b6
FelixA02 <56594071+FelixA02@users.noreply.github.com>
2022-11-28 08:21:19 -0500

    Update enleverMultiples.php

--- serveur/pages/circuits/enleverMultiples.php
+++ serveur/pages/circuits/enleverMultiples.php
@@ -1,44 +1,36 @@
 <?php
-	require_once("../../includes/configbd.inc.php");
-	$tabIdArticles=explode(";",$_POST['idaM']);//9;13;50
-    $taille = count($tabIdArticles);
-    
-    function enleverUnArticle($idArticle){
-        global $connexion;
-        $requete="SELECT imageart FROM articles WHERE ida=?";
-            $stmt = $connexion->prepare($requete);
-            $stmt->bind_param("i", $idArticle);
-            $stmt->execute();
-            $result = $stmt->get_result();
-            if(!$ligne = $result->fetch_object()){
-                mysqli_close($connexion);
-                header('Location: ../admin/admin.php?msg=Article+est+introuvable');
-                exit;
-            }
-            $image=$ligne->imageart;
-            if($image!="avatar.png"){
-                $rmImg='../../images_articles/'.$image;
-                $tabFichiers = glob('../../images_articles/*');
-                //print_r($tabFichiers);
-                //parcourir les fichier
-                foreach($tabFichiers as $fichier){
-                    if(is_file($fichier) && $fichier==trim($rmImg)) {
-                    // enlever le fichier
-                    unlink($fichier);
-                    break;
-                    //
-                    }
-                }
-            }
-            $requete="DELETE FROM articles WHERE ida=?";
-            $stmt = $connexion->prepare($requete);
-            $stmt->bind_param("i", $idArticle);
-            $stmt->execute();
+    require_once("../../includes/configbd.inc.php");
+    $idCircuit=$_POST['idc'];
+    $requete="SELECT * FROM circuits WHERE idc=?";
+    $stmt = $connexion->prepare($requete);
+    $stmt->bind_param("i", $idCircuit);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if(!$ligne = $result->fetch_object()){
+        mysqli_close($connexion);
+        header('Location: ../admin/admin.php?msg=Circuit+est+introuvable');
+        exit;
     }
-    for($i=0; $i<$taille; $i++){
-        enleverUnArticle($tabIdArticles[$i]);  
+    $image=$ligne->imageart;
+    if($image!="avatar.png"){
+        $rmImg='../../images_circuits/'.$image;
+        $tabFichiers = glob('../../images_circuits/*');
+        //print_r($tabFichiers);
+        //parcourir les fichier
+        foreach($tabFichiers as $fichier){
+            if(is_file($fichier) && $fichier==trim($rmImg)) {
+            // enlever le fichier
+            unlink($fichier);
+            break;
+            //
+            }
+        }
     }
-	mysqli_close($connexion);
-	header('Location: ../admin/admin.php?msg=Les+articles+choisis+ont+été+retirés');
-	exit;
+    $requete="DELETE FROM circuits WHERE idc=?";
+    $stmt = $connexion->prepare($requete);
+    $stmt->bind_param("i", $idCircuit);
+    $stmt->execute();
+    mysqli_close($connexion);
+    header('Location: ../admin/admin.php?msg=Circuit+a+été+enlevé');
+    exit;
 ?>
