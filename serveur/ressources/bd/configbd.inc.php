78eb854052e504ee4d164a86e3bc5d001f0f12b8
FelixA02 <56594071+FelixA02@users.noreply.github.com>
2022-11-29 14:52:05 -0500

    update config et connexion

--- serveur/ressources/bd/configbd.inc.php
+++ serveur/ressources/bd/configbd.inc.php
@@ -1,17 +1,6 @@
 <?php
-    define("SERVEUR","localhost");
-    define("USAGER","root");
-    define("PASSE","");
-    define("BD","bdprojetweb");
-    $connexion = new mysqli(SERVEUR,USAGER,PASSE,BD);
-    if ($connexion->connect_errno) {
-      echo "Problème de connexion au serveur de bd";
-      exit();
-	}
-  /*<?php
 	$SERVEUR = "localhost";
 	$USAGER = "root";
 	$PASS = "";
-	$BD = "a22bdfilms";
-?>*/
+	$BD = "bdprojetweb";
 ?>
