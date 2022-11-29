84b963aaaa848107059f7c8e7f43cb5e7949fdbc
FelixA02 <56594071+FelixA02@users.noreply.github.com>
2022-11-29 14:25:29 -0500

    finit DaoCircuit

--- serveur/modele/circuit/DaoCircuit.php
+++ serveur/modele/circuit/DaoCircuit.php
@@ -2,11 +2,11 @@
 // Au début de PHP: Déclarer les types dans les paramétres des fonctions
 declare (strict_types=1);
 
-require_once(__DIR__."/../ressources/bd/Connexion.php");
-require_once("Film.php");
+require_once(__DIR__."/../../ressources/bd/connexion.php");
+require_once("Circuit.php");
 
-class DaoFilm {
-    static private $modelFilm = null;
+class DaoCircuit {
+    static private $modelCircuit = null;
     private $reponse=array();
     private $connexion = null;
 	
@@ -15,48 +15,48 @@ class DaoFilm {
     }
     
 // Retourne le singleton du modèle 
-	static function  getDaoFilm():DaoFilm {
-		if(self::$modelFilm == null){
-			self::$modelFilm = new DaoFilm();  
+	static function  getDaoCircuit():DaoCircuit {
+		if(self::$modelCircuit == null){
+			self::$modelCircuit = new DaoCircuit();  
 		}
-		return self::$modelFilm;
+		return self::$modelCircuit;
 	}
 	
-	function MdlF_Enregistrer(Film $film):string {
+	function MdlC_Enregistrer(Circuit $circuit):string {
         //global $reponse;
        
         $connexion =  Connexion::getConnexion();
-        $requette="INSERT INTO films VALUES(0,?,?,?,?,?)";
+        $requette="INSERT INTO circuits VALUES(0,?,?,?,?)";
         try{
             
-            $donnees = [$film->getTitre(),$film->getDuree(),$film->getRealisateur(),$film->getPochette(),NULL];
+            $donnees = [$circuit->get_nom(),$circuit->get_photo(),$circuit->get_description(),$circuit->get_etat()];
             $stmt = $connexion->prepare($requette);
             $stmt->execute($donnees);
             $this->reponse['OK'] = true;
-            $this->reponse['msg'] = "Film bien enregistre";
+            $this->reponse['msg'] = "Circuit bien enregistre";
         }catch (Exception $e){
             $this->reponse['OK'] = false;
-            $this->reponse['msg'] = "Probléme pour enregistrer le film";
+            $this->reponse['msg'] = "Probléme pour enregistrer le Circuit";
         }finally {
           unset($connexion);
           return json_encode($this->reponse);
         }
     }
 	
-    function MdlF_getAll():string {
+    function MdlC_getAll():string {
         global $reponse;
         $connexion = Connexion::getConnexion();
-        $requette="SELECT * FROM films";
+        $requette="SELECT * FROM circuits";
         try{
             $stmt = $connexion->prepare($requette);
             $stmt->execute();
             $reponse['OK'] = true;
             $reponse['msg'] = "";
-            $reponse['listeFilms'] = array();
-            $reponse['listeFilms'] = $stmt->fetchAll();
+            $reponse['listeCircuits'] = array();
+            $reponse['listeCircuits'] = $stmt->fetchAll();
         }catch (Exception $e){ 
             $reponse['OK'] = false;
-            $reponse['msg'] = "Problème pour obtenir les données des films";
+            $reponse['msg'] = "Problème pour obtenir les données des circuits";
             //$reponse['msg'] = $e->getMessage();
         }finally {
           unset($connexion);
