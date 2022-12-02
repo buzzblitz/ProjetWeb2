83a4b94c5190d61f3e81801eace3379508ad9c55
felix <felix.gp@live.ca>
2022-12-01 19:34:32 -0500

    db

--- serveur/ressources/bd/populateBD.sql
+++ serveur/ressources/bd/populateBD.sql
@@ -4,17 +4,17 @@ INSERT INTO `journees` (`idj`, `ide`, `datej`, `descriptionj`) VALUES ('1', '1',
 INSERT INTO `activites` (`ida`, `idj`, `noma`, `tempsdebut`, `tempsfin`, `descriptiona`) VALUES ('1', '1', 'jouer au golf', '2022-11-16 14:30:41', '2022-11-16 14:30:42', 'une belle partie de 8trous');
 
 INSERT INTO `circuits` (`idc`, `nomc`, `photoc`, `descriptionc`, `etat`) VALUES ('2', 'Portugal', NULL, 'Voyage au Portugal', 'A');
-INSERT INTO `etapes` (`ide`, `idc`, `nome`, `photoe`, `descriptione`, `debut`, `fin`, `lieurencontre`) VALUES ('2', '2', 'Manger', NULL, 'Première étape du voyage au Portugal', '2022-12-25 18:00:00', '2022-12-25 20:00:00', 'Le restaurant');
+INSERT INTO `etapes` (`ide`, `idc`, `nome`, `photoe`, `descriptione`, `debut`, `fin`, `lieurencontre`) VALUES ('2', '2', 'Porto', NULL, 'Première étape du voyage au Portugal', '2022-12-25 18:00:00', '2022-12-25 20:00:00', 'Premiere journee Porto');
 INSERT INTO `journees` (`idj`, `ide`, `datej`, `descriptionj`) VALUES ('2', '2', '2022-12-25 12:00:00', 'Noel');
 INSERT INTO `activites` (`ida`, `idj`, `noma`, `tempsdebut`, `tempsfin`, `descriptiona`) VALUES ('2', '2', 'Restaurant Portugal', '2022-12-25 19:30:00', '2022-12-25 20:30:00', 'Degustation locale');
 
 INSERT INTO `circuits` (`idc`, `nomc`, `photoc`, `descriptionc`, `etat`) VALUES ('3', 'Australie', NULL, 'Voyage en Australie', 'A');
-INSERT INTO `etapes` (`ide`, `idc`, `nome`, `photoe`, `descriptione`, `debut`, `fin`, `lieurencontre`) VALUES ('3', '3', 'Manger', NULL, 'Première étape du voyage en Australie', '2022-12-25 18:00:00', '2022-12-25 20:00:00', 'Le restaurant');
+INSERT INTO `etapes` (`ide`, `idc`, `nome`, `photoe`, `descriptione`, `debut`, `fin`, `lieurencontre`) VALUES ('3', '3', 'Sydney', NULL, 'Première étape du voyage en Australie', '2022-12-25 18:00:00', '2022-12-25 20:00:00', 'Premiere journee a Sydney');
 INSERT INTO `journees` (`idj`, `ide`, `datej`, `descriptionj`) VALUES ('3', '3', '2022-12-25 12:00:00', 'Noel');
 INSERT INTO `activites` (`ida`, `idj`, `noma`, `tempsdebut`, `tempsfin`, `descriptiona`) VALUES ('3', '3', 'Restaurant Australien', '2022-12-25 19:30:00', '2022-12-25 20:30:00', 'Degustation locale');
 
 INSERT INTO `circuits` (`idc`, `nomc`, `photoc`, `descriptionc`, `etat`) VALUES ('4', 'Japon', NULL, 'Voyage au Japon', 'A');
-INSERT INTO `etapes` (`ide`, `idc`, `nome`, `photoe`, `descriptione`, `debut`, `fin`, `lieurencontre`) VALUES ('4', '4', 'Manger', NULL, 'Première étape du voyage au Japon', '2022-12-25 18:00:00', '2022-12-25 20:00:00', 'Le restaurant');
+INSERT INTO `etapes` (`ide`, `idc`, `nome`, `photoe`, `descriptione`, `debut`, `fin`, `lieurencontre`) VALUES ('4', '4', 'Tokyo', NULL, 'Première étape du voyage au Japon', '2022-12-25 18:00:00', '2022-12-25 20:00:00', 'Premiere journee a tokyo');
 INSERT INTO `journees` (`idj`, `ide`, `datej`, `descriptionj`) VALUES ('4', '4', '2022-12-25 12:00:00', 'Noel');
 INSERT INTO `activites` (`ida`, `idj`, `noma`, `tempsdebut`, `tempsfin`, `descriptiona`) VALUES ('4', '4', 'Restaurant Japon', '2022-12-25 19:30:00', '2022-12-25 20:30:00', 'Degustation locale');
 
