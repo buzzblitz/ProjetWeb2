686d64e3207c34aa9321d7d8b554cb10d28a14af
FelixA02 <56594071+FelixA02@users.noreply.github.com>
2022-12-03 15:04:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
     <script src="client/public/home/vueHome.js"></script>
 
 </head>
-<body onload="javascript: chargerCircuitsAJAX();">
+<body onload="javascript: chargerFilmsAJAX();">
     
      <?php 
       // include_once('serveur/includes/menu_accueil.inc.php'); 
@@ -31,7 +31,7 @@
         <!-- <nav class="navbar navbar-expand-lg bg-nav-perso"> -->
     <nav class="navbar navbar-expand-lg bg-nav-perso">
         <div class="container-fluid">
-            <a class="navbar-brand" href="javascript: chargerCircuitsAJAX();"></a>
+            <a class="navbar-brand" href="javascript: chargerFilmsAJAX();"></a>
             <img  src="serveur/ressources/images/logo_sanstexte.jpg" alt="Logo" width="60" height="50" class="d-inline-block align-text-top">
             <div id="logo_text" >The Luxury Traveler</div>
             </a>
@@ -43,7 +43,7 @@
                
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="javascript: chargerCircuitsAJAX();">Accueil</a>
+                        <a class="nav-link" href="javascript: chargerFilmsAJAX();">Accueil</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="javascript: montrerFormEnreg();">Enregistrer</a>
