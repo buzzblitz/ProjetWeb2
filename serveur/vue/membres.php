98aa16f79e66914e2a69fb29ec2ab36624865fc1
abder123alg <sid.ali.haddouni@hotmail.com>
2022-12-08 16:29:50 -0500

    Code Inutile plus photo membre

--- serveur/vue/membres.php
+++ serveur/vue/membres.php
@@ -13,7 +13,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Membre</title>
     <link rel="stylesheet" href="../../client/utilitaires/icons-1.8.1/bootstrap-icons.css">
     <link rel="stylesheet" href="../../client/utilitaires/bootstrap-5.1.3-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../../client/public/css/style.css">
